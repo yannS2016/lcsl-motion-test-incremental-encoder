c673ccb80034f55e673dc801d49eb2b08d48cc21
Yann Stephen Mandza <ysmandza@slac.stanford.edu>
2025-09-07 22:18:19 -0700

    Fixed missing tmc

--- iocBoot/ioc-lclsPLCMotionTest/st.cmd
+++ iocBoot/ioc-lclsPLCMotionTest/st.cmd
@@ -6,8 +6,8 @@
 # Generated using: pytmc 2.18.2
 # Project version: unknown
 #    Project hash: unknown
-#     PLC IP/host: 192.168.1.166
-#      PLC Net ID: 192.168.1.166.1.1
+#     PLC IP/host: 192.168.206.130
+#      PLC Net ID: 192.168.206.130.1.1
 # ** DEVELOPMENT MODE IOC **
 # * Using IOC boot directory for autosave.
 # * Archiver settings will not be configured.
@@ -35,8 +35,8 @@ dbLoadDatabase("$(ADS_IOC_TOP)/dbd/adsIoc.dbd")
 adsIoc_registerRecordDeviceDriver(pdbbase)
 
 epicsEnvSet("ASYN_PORT",        "ASYN_PLC")
-epicsEnvSet("IPADDR",           "192.168.1.166")
-epicsEnvSet("AMSID",            "192.168.1.166.1.1")
+epicsEnvSet("IPADDR",           "192.168.206.130")
+epicsEnvSet("AMSID",            "192.168.206.130.1.1")
 epicsEnvSet("AMS_PORT",         "851")
 epicsEnvSet("ADS_MAX_PARAMS",   "1178")
 epicsEnvSet("ADS_SAMPLE_MS",    "50")
@@ -45,7 +45,7 @@ epicsEnvSet("ADS_TIMEOUT_MS",   "1000")
 epicsEnvSet("ADS_TIME_SOURCE",  "0")
 
 # Add a route to the PLC automatically:
-system("${ADS_IOC_TOP}/scripts/add_route.sh 192.168.1.166 ^172.*$")
+system("${ADS_IOC_TOP}/scripts/add_route.sh 192.168.206.130 ^172.*$")
 
 # adsAsynPortDriverConfigure(portName, ipaddr, amsaddr, amsport,
 #    asynParamTableSize, priority, noAutoConnect, defaultSampleTimeMS,
@@ -132,7 +132,7 @@ dbLoadRecords("caPutLog.db", "IOC=$(IOC)")
 dbLoadRecords("TwinCAT_TaskInfo.db", "PORT=$(ASYN_PORT),PREFIX=PLC:lclsPLCMotionTest,IDX=1,TASK_PORT=350")
 dbLoadRecords("TwinCAT_AppInfo.db", "PORT=$(ASYN_PORT), PREFIX=PLC:lclsPLCMotionTest")
 
-dbLoadRecords("TwinCAT_Project.db", "PREFIX=PLC:lclsPLCMotionTest,PROJECT=lcls-motion-test.tsproj,HASH=unknown,VERSION=unknown,PYTMC=2.18.2,PLC_HOST=192.168.1.166")
+dbLoadRecords("TwinCAT_Project.db", "PREFIX=PLC:lclsPLCMotionTest,PROJECT=lcls-motion-test.tsproj,HASH=unknown,VERSION=unknown,PYTMC=2.18.2,PLC_HOST=192.168.206.130")
 
 #   LCLS General: * (SLAC)
 dbLoadRecords("TwinCAT_Dependency.db", "PREFIX=PLC:lclsPLCMotionTest,DEPENDENCY=LCLS_General,VERSION=*,VENDOR=SLAC")
